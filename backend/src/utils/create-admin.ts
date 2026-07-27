60f79e4e54a5725bc9058719a0514b9bd1cfb1de
Nikhil <nikhil45@gmail.com>
2026-07-27 22:13:48 +0545

    feat(backend): add admin account creation utility script

--- backend/src/utils/create-admin.ts
+++ backend/src/utils/create-admin.ts
@@ -1,18 +1,20 @@
 import UserModel from "../models/user.model";
+import { UserRole } from "../types/user.type";
 import connectDB from "../database/mongodb";
 
 async function updateUserToAdmin() {
   await connectDB();
-  
+
   const user = await UserModel.findOne({ email: "admin@bikesewa.com" });
   if (user) {
-    user.role = "admin";
+    user.role = UserRole.ADMIN;
     await user.save();
     console.log("Updated admin@bikesewa.com to admin role");
+    process.exit(0);
   } else {
     console.log("Admin user not found");
+    process.exit(1);
   }
-  process.exit(0);
 }
 
 updateUserToAdmin().catch((err) => {
