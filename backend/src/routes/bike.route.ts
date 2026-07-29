c45d9c5d738f3e4fe2486863ed44146c56daa573
Nikhil <nikhil45@gmail.com>
2026-07-29 12:47:35 +0545

    feat(backend): wire up admin routes for bike module

--- backend/src/routes/bike.route.ts
+++ backend/src/routes/bike.route.ts
@@ -1,14 +1,23 @@
 import { Router } from "express";
 import bikeController from "../controllers/bike.controller";
 import authorizedMiddleware from "../middlewares/authorized.middleware";
+import adminMiddleware from "../middlewares/admin.middleware";
 
 const bikeRouter = Router();
 
+// Public routes
 bikeRouter.get("/", bikeController.getAllBikes);
 bikeRouter.get("/search", bikeController.searchBikes);
 bikeRouter.get("/location", bikeController.getBikesByLocation);
 bikeRouter.get("/:id", bikeController.getBikeById);
+
+// Protected routes (authenticated users)
 bikeRouter.post("/:id/rent", authorizedMiddleware, bikeController.rentBike);
 bikeRouter.post("/:id/return", authorizedMiddleware, bikeController.returnBike);
+
+// Admin routes
+bikeRouter.post("/", authorizedMiddleware, adminMiddleware, bikeController.adminCreateBike);
+bikeRouter.put("/:id", authorizedMiddleware, adminMiddleware, bikeController.adminUpdateBike);
+bikeRouter.delete("/:id", authorizedMiddleware, adminMiddleware, bikeController.adminDeleteBike);
 
 export default bikeRouter;
